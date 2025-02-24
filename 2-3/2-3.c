2bf249a50eea91d9f787d562b85586daf93cc214
namwoo <gotree94@gmail.com>
2025-02-24 19:31:51 +0900

    2-5 commit

---
+++ 2-3/2-3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Be happy\n");
+	printf("12345678901234657890\n");
+	printf("My\tfriend\n");
+	printf("Goodt\bd\tchance\n");
+	printf("Cow\rW\a\n");
+
+	return 0;
+}
